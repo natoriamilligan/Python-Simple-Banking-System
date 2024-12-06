0b7764eb3edbe56714e0bf986433e6fe1479e765
Natoria Ray <46578731+natoriaray@users.noreply.github.com>
2024-12-06 10:26:26 -0600

    Added username and password functionality

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ with open("accounts.json", "r") as file:
     accounts = json.load(file)
     print(accounts)
 
-def create_account(name, pin, bal):
-  accounts[pin] = {"name": name, "balance": bal}
+def create_account(username, pin, bal):
+  accounts[username] = {"username": username, "pin": pin, "balance": bal}
   with open("accounts.json", "w") as file:
     json.dump(accounts, file)
 
@@ -14,14 +14,19 @@ login = input("Do you have an account? y or n: ").lower()
 if login == "y":
   pass
 elif login == "n":
-  user_name = input("What is your first name?: ").lower()
+  username = input("Please create a username: ").lower()
+  if username in accounts:
+    username = input("That username has been taken. Please create a username: ").lower()
   pin_num = input("Please create a 4 digit pin: ")
+  
   deposit = input("Would you like to submit an initial deposit? y or n").lower()
+
   if deposit == "y":
     balance = int(input("Please enter the amount you would like to deposit (only numbers no signs)"))
   else:
     balance = 0
-  create_account(user_name, pin_num, balance)
+
+  create_account(username, pin_num, balance)
   print(accounts)
 else:
   print("Your input was invalid. Please try again.")
