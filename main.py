97c5cec5278266be8b04201c8892769c5a9e5b1c
Natoria Ray <46578731+natoriaray@users.noreply.github.com>
2024-12-12 09:31:14 -0600

    Created transaction history in user profile

--- main.py
+++ main.py
@@ -11,25 +11,25 @@ def create_account():
   username = input("Please create a username: ").lower()
   if username in accounts:
     username = input("That username has been taken. Please create a username: ").lower()
-  pin_num = input("Please create a 4 digit pin: ")
-
-  deposit = input("Would you like to submit an initial deposit? y or n").lower()
+  else: 
+    pin_num = input("Please create a 4 digit pin: ")
+    deposit = input("Would you like to submit an initial deposit? y or n").lower()
 
   if deposit == "y":
-    balance = float(input("Please enter the amount you would like to deposit (only numbers no signs)"))
-    print(f"Your new balance is: ${accounts[username]['balance']:.2f}.")
+     balance = float(input("Please enter the amount you would like to deposit (only numbers no signs)"))
+     history = {"type": "deposit", "amount": balance }
   elif deposit == "n":
     balance = 0
   else:
-    print("Input was invalid. Try making a deposit later")
-    balance = 0
-  accounts[username] = {"username": username, "pin": pin_num, "balance": balance}
+    print("Input was invalid. Please start over")
+    create_account()
+  accounts[username] = {"username": username, "pin": pin_num, "balance": balance, "history": [history]}
   with open("accounts.json", "w") as file:
     json.dump(accounts, file)
 
 def access_account(user):
   while True:
-    action = input("Would you like to make a deposit or a withdrawl? d or w").lower()
+    action = input("Would you like to make a deposit, withdrawal, transfer, or view transaction history? d, w, t, or v").lower()
     if action == "d":
       deposit_amt = float(input("How much would you like to deposit?"))
       new_balance = float(accounts[user]["balance"] + deposit_amt)
@@ -37,7 +37,7 @@ def access_account(user):
 
       with open("accounts.json", "w") as file:
         json.dump(accounts, file)
-      
+
       print(f"Your new balance is: ${accounts[user]['balance']:.2f}.")
       user_active = ("Would you like to logout or do another action? Type action or logout.").lower()
       if user_active == "action":
@@ -54,12 +54,12 @@ def access_account(user):
       if new_balance < 0:
         print("You do not have enough funds to withdrawl. Please try again")
         continue
-      else:  
+      else:
         accounts[user]["balance"] = new_balance
 
         with open("accounts.json", "w") as file:
           json.dump(accounts, file)
-        
+
         print(f"Your new balance is: ${accounts[user]['balance']:.2f}.")
         user_active = input("Would you like to logout or do another action? Type action or logout.").lower()
         if user_active == "action":
@@ -70,6 +70,8 @@ def access_account(user):
         else:
           print("Invalid input. You have been logged out.")
           break
+    elif action == "v":
+      pass
     else:
       print("You entered an invalid input.")
       continue
@@ -83,13 +85,13 @@ def login():
         try_again = input("User invalid. Would you like to create account or retype your username? Type create or retype:").lower()
         if try_again == "create":
            create_account()
-           print("You've successfully created your account!")
+           print(f"You've successfully created your account! Your new balance is: ${accounts[user_valid]['balance']:.2f}.")
            break
         elif try_again == "retype":
           continue
         else:
           try_again = input("Invalid input. Would you like to create account or retype your username? Type create or retype:").lower()
-      else: 
+      else:
         pin_valid = input("What is your pin? :")
 
         if user_valid in accounts and pin_valid == accounts[user_valid]["pin"]:
@@ -101,6 +103,7 @@ def login():
       create_account()
       break
     else:
-      print("Your input was invalid. Please try again.")
+      print("Your input was invalid. Please try again later.")
+      break
 
 login()
