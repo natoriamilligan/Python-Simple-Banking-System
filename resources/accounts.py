0506868c309ffb596a1f678baa875f96f67df659
Natoria Milligan <natoriaray@gmail.com>
2024-12-24 15:52:46 -0600

    created blueprint for account

--- resources/accounts.py
+++ resources/accounts.py
@@ -1,3 +1,7 @@
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+
+blp = Blueprint("accounts", __name__, description="Operation on accounts")
+
+        
