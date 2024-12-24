1e507af8ccb027aaa4c49ce0a07a0a5bf422668c
Natoria Milligan <natoriaray@gmail.com>
2024-12-24 15:52:15 -0600

    added 2 more columns to account model

--- models/accounts.py
+++ models/accounts.py
@@ -4,6 +4,8 @@ class AccountModel(db.Model):
     __tablename__ = "accounts"
 
     id = db.Column(db.Integer, primary_key=True)
+    first_namme = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     balance = db.Column(db.Float(precision=2), unique=False, nullable=False)
