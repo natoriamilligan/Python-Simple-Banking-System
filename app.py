8e4989c44e2869998b4580bf8c592ff953670cd7
Natoria Milligan <natoriaray@gmail.com>
2024-12-23 18:08:49 -0600

    added configurations for flask app and database

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_smorest import Api
+from db import db
 
 app = Flask(__name__)
 
 def create_app():
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    
     api = Api(app)
