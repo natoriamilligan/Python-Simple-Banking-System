b42c9bf06fb5fa75035763a4669d7412451826bb
Natoria Milligan <natoriaray@gmail.com>
2024-12-27 09:29:23 -0600

    added routes for accounts resource

--- resources/accounts.py
+++ resources/accounts.py
@@ -4,4 +4,21 @@ from flask_smorest import Blueprint, abort
 
 blp = Blueprint("accounts", __name__, description="Operation on accounts")
 
-        
+@blp.route("/account")
+class AccountList(MethodView):
+    def get(self):
+        pass
+
+    def post(self, account_data):
+        pass
+
+@blp.route("/account/<int:account_id>")
+class Account(MethodView):
+    def get(self, account_id):
+        pass
+
+    def put(self, account_data, account_id):
+        pass
+
+    def delete(self, account_id):
+        pass
