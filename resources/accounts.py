4de2181110c493bc1336812c1da804b90130b8e4
Natoria Milligan <natoriaray@gmail.com>
2024-12-27 13:44:53 -0600

    created routes for accounts

--- resources/accounts.py
+++ resources/accounts.py
@@ -1,16 +1,28 @@
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
+
+from db import db
+from models import AccountModel
 
 blp = Blueprint("accounts", __name__, description="Operation on accounts")
 
 @blp.route("/account")
 class AccountList(MethodView):
     def get(self):
-        pass
+        return AccountModel.query.all()
 
     def post(self, account_data):
-        pass
+        account = AccountModel(**account_data)
+
+        try:
+            db.session.add(account)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occured while inserting the item into the database")
+
+        return account
 
 @blp.route("/account/<int:account_id>")
 class Account(MethodView):
