c66e3c7bb464e77b10ec3e1960ff90de310291f4
Natoria Milligan <natoriaray@gmail.com>
2024-12-27 10:41:52 -0600

    created routes for transaction resource

--- resources/transactions.py
+++ resources/transactions.py
@@ -3,3 +3,18 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
 blp = Blueprint("transactions", __name__, description="Operation on transactions")
+
+@blp.route("/account/<int:account_id>/transaction")
+class AccountTransactions(MethodView):
+    def get(self, account_id):
+        #get all transactions from a specific account
+        pass
+
+    def post(self, account_data, account_id):
+        #depending on the type of transaction will post to the specific account
+        pass
+
+@blp.route("/transaction/<int:transaction_id>")
+class Transaction(MethodView):
+    def get(self, transaction_id):
+        pass
