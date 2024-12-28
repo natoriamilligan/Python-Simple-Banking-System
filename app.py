d1bb55fb59174abbb5293000240ee606a3bef8d4
Natoria Milligan <natoriaray@gmail.com>
2024-12-28 11:26:57 -0600

    moved a line inside create app function

--- app.py
+++ app.py
@@ -9,12 +9,19 @@ from resources.transactions import blp as TransactionsBlueprint
 
 load_dotenv()
 
-app = Flask(__name__)
-
 def create_app():
+    app = Flask(__name__)
+
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Banking API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
     db.init_app(app)
 
     api = Api(app)
@@ -25,5 +32,8 @@ def create_app():
     with app.app_context():
         db.create_all()
 
+
     return app
+
+
     
