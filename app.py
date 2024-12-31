7c1059a26553442677c88562b5c692fe0fcf9111
Natoria Milligan <natoriaray@gmail.com>
2024-12-30 18:31:03 -0600

    deleted some configurations and deleted dotenv since i will use sqlite for development

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-from dotenv import load_dotenv
 from flask import Flask
 from flask_smorest import Api
 from db import db
@@ -7,20 +6,15 @@ from db import db
 from resources.accounts import blp as AccountsBlueprint
 from resources.transactions import blp as TransactionsBlueprint
 
-load_dotenv()
-
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Banking API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
-    app.config["OPENAPI_URL_PREFIX"] = "/"
-    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
     db.init_app(app)
 
