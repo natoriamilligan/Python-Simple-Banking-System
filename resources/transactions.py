efbcfd746f041c88b3b51d80b955e6d0dbe482ac
Natoria Milligan <natoriaray@gmail.com>
2025-01-02 10:56:08 -0600

    create first route for transactions. Get all transactions for a specific account

--- resources/transactions.py
+++ resources/transactions.py
@@ -2,13 +2,15 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
+from models import AccountModel
+
 blp = Blueprint("transactions", __name__, description="Operation on transactions")
 
 @blp.route("/account/<int:account_id>/transaction")
 class AccountTransactions(MethodView):
     def get(self, account_id):
-        #get all transactions from a specific account
-        pass
+        account = AccountModel.query.get_or_404(account_id)
+        return account.transactions.all()
 
     def post(self, account_data, account_id):
         #depending on the type of transaction will post to the specific account
