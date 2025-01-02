ba08fa71a0609dcf315ffbca9bd1897a24d9eff5
Natoria Milligan <natoriaray@gmail.com>
2025-01-02 10:26:37 -0600

    deleted json file

--- models/accounts.py
+++ models/accounts.py
@@ -4,9 +4,9 @@ class AccountModel(db.Model):
     __tablename__ = "accounts"
 
     id = db.Column(db.Integer, primary_key=True)
-    first_namme = db.Column(db.String(80), unique=False, nullable=False)
+    first_name = db.Column(db.String(80), unique=False, nullable=False)
     last_name = db.Column(db.String(80), unique=False, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    balance = db.Column(db.Float(precision=2), unique=False, nullable=False)
+    balance = db.Column(db.Float(precision=2), default=0, unique=False, nullable=False)
     transactions = db.relationship("TransactionModel", back_populates="account", lazy="dynamic", cascade="all, delete")
