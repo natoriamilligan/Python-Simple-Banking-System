f97b0182d44c06040cde7396ab21815547898367
Natoria Milligan <natoriaray@gmail.com>
2025-01-03 09:01:17 -0600

    created input for deposits and withdrawals

--- resources/transactions.py
+++ resources/transactions.py
@@ -13,8 +13,20 @@ class AccountTransactions(MethodView):
         return account.transactions.all()
 
     def post(self, account_data, account_id):
-        #depending on the type of transaction will post to the specific account
-        pass
+        account = AccountModel.query.get_or_404(account_id)
+
+        if account_data["type"] == "deposit":
+            account.balance = account.balance + account_data["amount"]
+        elif account_data["type"] == "withdrawal":
+            if account.balance - account_data["amount"] < 0:
+                abort(422, message="Not enough funds in the account")
+            else:
+                account.balance = account.balance - account_data["amount"]
+        else:
+            abort(400, message="Invalid JSON payload")
+
+        return {"message": f"Your new balance is ${account.balance}"}
+
 
 @blp.route("/transaction/<int:transaction_id>")
 class Transaction(MethodView):
