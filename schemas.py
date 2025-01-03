6ba9448ce77900c2bb7e30dd77b51c7f4acdd01f
Natoria Milligan <natoriaray@gmail.com>
2025-01-03 10:25:15 -0600

    changed schemas to reflect receiving and submitting transactions. Will make transfer logic make sense later on

--- schemas.py
+++ schemas.py
@@ -4,8 +4,8 @@ class TransactionSchema(Schema):
     id = fields.Int(dump_only=True)
     type = fields.Str(required=True)
     amount = fields.Float(required=True)
-    recipient_account_id = fields.Integer(required=True)
-    account_id = fields.Int(required=True)
+    recipient_id = fields.Int(required=True)
+    submitter_id = fields.Int(required=True)
 
 class UpdateAccountSchema(Schema):
     first_name = fields.Str(required=True)
@@ -16,4 +16,5 @@ class AccountSchema(UpdateAccountSchema):
     id = fields.Int(dump_only=True)
     username = fields.Str(required=True)
     balance = fields.Str(dump_only=True)
-    transactions = fields.List(fields.Nested(TransactionSchema()), dump_only=True)
+    received_transactions = fields.List(fields.Nested(TransactionSchema()), dump_only=True)
+    submitted_transactions = fields.List(fields.Nested(TransactionSchema()), dump_only=True)
