c690d94200fb589f8dcf522b6c164e9a66a505d7
Natoria Milligan <natoriaray@gmail.com>
2025-01-03 11:18:40 -0600

    imported transaction model

--- models/accounts.py
+++ models/accounts.py
@@ -1,4 +1,5 @@
 from db import db
+from models.transactions import TransactionModel
 
 class AccountModel(db.Model):
     __tablename__ = "accounts"
@@ -9,5 +10,5 @@ class AccountModel(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     balance = db.Column(db.Float(precision=2), default=0, unique=False, nullable=False)
-    received_transactions = db.relationship("TransactionModel", foreign_keys=["TransactionModel.recipient_id"], back_populates="recipient", lazy="dynamic", cascade="all, delete")
-    submitted_transactions = db.relationship("TransactionModel", foreign_keys=["TransactionModel.submitter_id"], back_populates="submitter", lazy="dynamic", cascade="all, delete")
+    received_transactions = db.relationship("TransactionModel", foreign_keys=[TransactionModel.recipient_id], back_populates="recipient", lazy="dynamic", cascade="all, delete")
+    submitted_transactions = db.relationship("TransactionModel", foreign_keys=[TransactionModel.submitter_id], back_populates="submitter", lazy="dynamic", cascade="all, delete")
