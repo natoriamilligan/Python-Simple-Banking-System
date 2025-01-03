2d78e9c994f265bc17c6768b767ffcd31f35144a
Natoria Milligan <natoriaray@gmail.com>
2025-01-03 10:23:27 -0600

    changed models to reflect receiving and submitting transactions. Will make transfer logic make sense later on

--- models/accounts.py
+++ models/accounts.py
@@ -9,4 +9,5 @@ class AccountModel(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     balance = db.Column(db.Float(precision=2), default=0, unique=False, nullable=False)
-    transactions = db.relationship("TransactionModel", back_populates="account", lazy="dynamic", cascade="all, delete")
+    received_transactions = db.relationship("TransactionModel", foreign_keys=["TransactionModel.recipient_id"], back_populates="recipient", lazy="dynamic", cascade="all, delete")
+    submitted_transactions = db.relationship("TransactionModel", foreign_keys=["TransactionModel.submitter_id"], back_populates="submitter", lazy="dynamic", cascade="all, delete")
