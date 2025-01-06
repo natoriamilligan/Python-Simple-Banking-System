610c98e4bbf692bf3fdb1eeee4de0536e3f03b70
Natoria Milligan <natoriaray@gmail.com>
2025-01-06 15:09:39 -0600

    added columns

--- models/transfers.py
+++ models/transfers.py
@@ -2,3 +2,11 @@ from db import db
 
 class TransferModel(db.Model):
     __tablename__ = "transfers"
+
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(80), unique=False, nullable=False)
+    amount = db.Column(db.Float(precision=2), unique=False, nullable=False)
+    submitter_id = db.Column(db.Integer, db.ForeignKey("accounts.id"), unique=False, nullable=False)
+    recipient_id = db.Column(db.Integer, db.ForeignKey("accounts.id"), unique=False, nullable=False)
+    submitter = db.relationship("AccountModel", foreign_keys=[submitter_id], back_populates="sent_transfers")
+    recipient = db.relationship("AccountModel", foreign_keys=[recipient_id], back_populates="received_transfers")
