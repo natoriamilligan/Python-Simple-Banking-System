d46b072caa2d10624b2cd5cbb85e9119c88416ef
Natoria Milligan <natoriaray@gmail.com>
2025-01-06 15:26:04 -0600

    added deposit and transfer schemas

--- schemas.py
+++ schemas.py
@@ -1,11 +1,17 @@
 from marshmallow import Schema, fields
 
-class TransactionSchema(Schema):
+class DepositSchema(Schema):
     id = fields.Int(dump_only=True)
     type = fields.Str(required=True)
     amount = fields.Float(required=True)
-    recipient_id = fields.Int(required=True)
+    account_id = fields.Int(required=True)
+
+class TransferSchema(Schema):
+    id = fields.Int(dump_only=True)
+    type = fields.Str(required=True)
+    amount = fields.Float(required=True)
     submitter_id = fields.Int(required=True)
+    recipient_id = fields.Int(required=True)
 
 class UpdateAccountSchema(Schema):
     first_name = fields.Str(required=True)
@@ -16,5 +22,6 @@ class AccountSchema(UpdateAccountSchema):
     id = fields.Int(dump_only=True)
     username = fields.Str(required=True)
     balance = fields.Str(dump_only=True)
-    received_transactions = fields.List(fields.Nested(TransactionSchema()), dump_only=True)
-    submitted_transactions = fields.List(fields.Nested(TransactionSchema()), dump_only=True)
+    sent_transfers = fields.List(fields.Nested(TransferSchema()), dump_only=True)
+    received_transfers = fields.List(fields.Nested(TransferSchema()), dump_only=True)
+    deposits = fields.List(fields.Nested(DepositSchema()), dump_only=True)
