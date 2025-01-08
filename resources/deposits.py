226ae5e1ef98bf202be7fa0f3dd9fe81a251cd3a
Natoria Milligan <natoriaray@gmail.com>
2025-01-07 18:29:25 -0600

    changed route for creating a deposit and removed old logic

--- resources/deposits.py
+++ resources/deposits.py
@@ -15,6 +15,27 @@ class AccountDeposit(MethodView):
     def get(self, account_id):
         account = AccountModel.query.get_or_404(account_id)
         return account.deposits.all()
+    
+    @blp.arguments(DepositSchema)
+    @blp.response(200, DepositSchema)
+    def post(self, deposit_data, account_id):
+        if AccountModel.query.filter(AccountModel.id == account_id).first():
+            account = AccountModel.query.get_or_404(account_id)
+            account.balance = account.balance + deposit_data["amount"]
+        else:
+            abort(404, message="Recipient not found")
+
+        deposit = DepositModel(account_id=account_id, **deposit_data)
+        
+        try:
+            db.session.add(deposit)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occured adding the deposit to the database")
+
+        return deposit
+    
+
 
 @blp.route("/deposit/<int:deposit_id>")
 class Deposit(MethodView):
@@ -23,46 +44,4 @@ class Deposit(MethodView):
         deposit = DepositModel.query.get_or_404(deposit_id)
         return deposit
     
-@blp.route("/transaction")
-class CreateAccountTransactions(MethodView):
 
-    def post(self, transaction_data):
-        pass
-    
-    """
-    @blp.arguments(TransactionSchema)
-    @blp.response(200, TransactionSchema)
-    def post(self, transaction_data):
-        if AccountModel.query.filter(AccountModel.id == transaction_data["recipient_id"]).first():
-            submitter_account = AccountModel.query.get_or_404(transaction_data["submitter_id"])
-            if transaction_data["type"] == "deposit":
-                submitter_account.balance = submitter_account.balance + transaction_data["amount"]
-            elif transaction_data["type"] == "withdrawal":
-                if submitter_account.balance - transaction_data["amount"] < 0:
-                    abort(422, message="Not enough funds in the account")
-                else:
-                    submitter_account.balance = submitter_account.balance - transaction_data["amount"]
-            elif transaction_data["type"] == "transfer":
-                if submitter_account.balance - transaction_data["amount"] < 0:
-                    abort(422, message="Not enough funds in the account")
-                else:
-                    submitter_account.balance = submitter_account.balance - transaction_data["amount"]
-                    
-                    recipient_account = AccountModel.query.get_or_404(transaction_data["recipient_id"])
-                    recipient_account.balance = recipient_account.balance + transaction_data["amount"]
-            else:
-                abort(400, message="Invalid JSON payload")
-        else:
-            abort(404, message="Recipient not found")
-
-        transaction = TransactionModel(**transaction_data)
-        
-        try:
-            db.session.add(transaction)
-            db.session.commit()
-        except SQLAlchemyError:
-            abort(500, message="An error occured adding the transaction to the database")
-
-        return transaction
-
-        """
