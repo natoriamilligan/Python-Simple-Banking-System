a581030f5ec733c39dee2d0734a63f43a998363b
Natoria Milligan <natoriaray@gmail.com>
2025-01-07 18:28:42 -0600

    deleted type column from models

--- models/transfers.py
+++ models/transfers.py
@@ -4,7 +4,6 @@ class TransferModel(db.Model):
     __tablename__ = "transfers"
 
     id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(80), unique=False, nullable=False)
     amount = db.Column(db.Float(precision=2), unique=False, nullable=False)
     submitter_id = db.Column(db.Integer, db.ForeignKey("accounts.id"), unique=False, nullable=False)
     recipient_id = db.Column(db.Integer, db.ForeignKey("accounts.id"), unique=False, nullable=False)
--- models/withdrawals.py
+++ models/withdrawals.py
@@ -4,7 +4,6 @@ class WithdrawalModel(db.Model):
     __tablename__ = "withdrawals"
 
     id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(80), unique=False, nullable=False)
     amount = db.Column(db.Float(precision=2), unique=False, nullable=False)
     account_id = db.Column(db.Integer, db.ForeignKey("accounts.id"), nullable=False)
     account = db.relationship("AccountModel", back_populates="withdrawals")
