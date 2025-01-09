0552193a0ab459f8118346510f9a43d9f980c205
Natoria Milligan <natoriaray@gmail.com>
2025-01-09 09:18:20 -0600

    finished all routes for withdrawals

--- resources/withdrawals.py
+++ resources/withdrawals.py
@@ -15,6 +15,28 @@ class AccountWithdrawal(MethodView):
     def get(self, account_id):
         account = AccountModel.query.get_or_404(account_id)
         return account.withdrawals.all()
+    
+    @blp.arguments(WithdrawalSchema)
+    @blp.response(200, WithdrawalSchema)
+    def post(self, withdrawal_data, account_id):
+        if AccountModel.query.filter(AccountModel.id == account_id).first():
+            account = AccountModel.query.get_or_404(account_id)
+            if account.balance - withdrawal_data["amount"] < 0:
+                abort(422, message="Not enough funds in the account")
+            else:
+                account.balance = account.balance - withdrawal_data["amount"]
+        else:
+            abort(404, message="Recipient not found")
+
+        withdrawal = WithdrawalModel(account_id=account_id, **withdrawal_data)
+        
+        try:
+            db.session.add(withdrawal)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occured adding the withdrawal to the database")
+
+        return withdrawal
 
 @blp.route("/withdrawal/<int:withdrawal_id>")
 class Deposit(MethodView):
