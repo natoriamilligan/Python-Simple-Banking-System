f3888cee627a79680e710055073627b0fc928d8d
Natoria Milligan <natoriaray@gmail.com>
2025-01-09 14:52:20 -0600

    completed route for creating transfers

--- resources/transfers.py
+++ resources/transfers.py
@@ -23,3 +23,32 @@ class AccountReceivedtransfer(MethodView):
         account = TransferModel.query.get_or_404(account_id)
         return account.received_transfers.all()
     
+@blp.route("/transfer")
+class AccountTransfer(MethodView):
+    @blp.arguments(TransferSchema)
+    @blp.response(200, TransferSchema)
+    def post(self, transfer_data):
+
+        if transfer_data["submitter_id"] == transfer_data["recipient_id"]:
+            abort(400, message="The submitter cannot be the same as the reciever.")
+        else:
+            submitter = AccountModel.query.get_or_404(transfer_data["submitter_id"])
+            recipient = AccountModel.query.get_or_404(transfer_data["recipient_id"])
+
+            if submitter.balance - transfer_data["amount"] < 0:
+                abort(422, message="Not enough funds in the submitter's account")
+            else:
+                submitter.balance = submitter.balance - transfer_data["amount"]
+                recipient.balance = recipient.balance + transfer_data["amount"]
+
+            transfer = TransferModel(**transfer_data)
+            
+            try:
+                db.session.add(transfer)
+                db.session.commit()
+            except SQLAlchemyError:
+                abort(500, message="An error occured adding the transaction to the database")
+
+            return transfer
+
+        
