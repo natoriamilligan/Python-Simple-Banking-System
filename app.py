70bc1ea364a7a9092a2ad9698b90797e32df7323
Natoria Milligan <natoriaray@gmail.com>
2025-01-10 12:45:39 -0600

    added better status codes for JWT

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
 from db import db
 from flask_jwt_extended import JWTManager
@@ -25,6 +25,34 @@ def create_app():
 
     app.config["JWT_SECRET_KEY"] = "79023088310581544527589837667420155225"
     jwt = JWTManager(app)
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
+    
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
+    
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )
 
     api.register_blueprint(AccountsBlueprint)
     api.register_blueprint(DepositsBlueprint)
