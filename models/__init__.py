c01fb5992e81c620fba315139223b69fa1d2233d
Natoria Milligan <natoriaray@gmail.com>
2025-01-14 14:46:26 -0600

    added Blocklist model to init file

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from models.accounts import AccountModel
 from models.deposits import DepositModel
 from models.transfers import TransferModel
 from models.withdrawals import WithdrawalModel
+from models.blocklist import BlocklistModel
